ecdc8c67bea67f01d0f9af478427b64029fea8b5
rezhariansyah <rezharatulangi@gmail.com>
2020-07-10 09:33:35 +0800

    bismillah

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "assets/scss/style.scss";
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
