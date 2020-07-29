22079c562834456d089c2418cd997014f2f80611
rezhariansyah <rezharatulangi@gmail.com>
2020-07-29 19:47:34 +0800

    test

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -3,18 +3,18 @@ import Button from "elements/Button";
 
 export default function MostPicked(props) {
   return (
-    <section className="container">
+    <section ref={props.refMostPicked} className="container">
       <h4 className="mb-3">Most Picked</h4>
       <div className="container-grid">
         {props.data.map((item, index) => {
           return (
-            <div
+            <div key={`mostpicked - ${index}`}
               className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
             >
               <div className="card card-featured">
                 <div className="tag">
                   ${item.price}
-                  <span className="font-weight-light">per ${item.unit}</span>
+                  <span className="font-weight-light"> / {item.unit}</span>
                 </div>
                 <figure className="img-wrapper">
                   <img
