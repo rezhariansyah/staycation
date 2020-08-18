94f4bf8fab9016ac5599917a8d73d2097c49c45b
rezhariansyah <rezharatulangi@gmail.com>
2020-08-19 01:45:55 +0700

    add reveal each component

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,45 +1,51 @@
 import React from "react";
 import Button from "elements/Button";
+import Fade from "react-reveal/Fade";
 
 export default function MostPicked(props) {
   return (
     <section ref={props.refMostPicked} className="container">
-      <h4 className="mb-3">Most Picked</h4>
-      <div className="container-grid">
-        {props.data.map((item, index) => {
-          return (
-            <div key={`mostpicked - ${index}`}
-              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
-            >
-              <div className="card card-featured">
-                <div className="tag">
-                  ${item.price}
-                  <span className="font-weight-light"> / {item.unit}</span>
-                </div>
-                <figure className="img-wrapper">
-                  <img
-                    src={item.imageUrl}
-                    className="img-cover"
-                    alt={item.name}
-                  />
-                </figure>
-                <div className="meta-wrapper">
-                  <Button
-                    type="link"
-                    href={`/properties/${item._id}`}
-                    className="stretched-link d-block text-white"
-                  >
-                    <h5>{item.name}</h5>
-                  </Button>
-                  <span>
-                    {item.city}, {item.country}
-                  </span>
-                </div>
+      <Fade bottom>
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="container-grid">
+          {props.data.map((item, index) => {
+            return (
+              <div
+                key={`mostpicked - ${index}`}
+                className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+              >
+                <Fade bottom delay={500*index}>
+                  <div className="card card-featured">
+                    <div className="tag">
+                      ${item.price}
+                      <span className="font-weight-light"> / {item.unit}</span>
+                    </div>
+                    <figure className="img-wrapper">
+                      <img
+                        src={item.imageUrl}
+                        className="img-cover"
+                        alt={item.name}
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        type="link"
+                        href={`/properties/${item._id}`}
+                        className="stretched-link d-block text-white"
+                      >
+                        <h5>{item.name}</h5>
+                      </Button>
+                      <span>
+                        {item.city}, {item.country}
+                      </span>
+                    </div>
+                  </div>
+                </Fade>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </Fade>
     </section>
   );
 }
